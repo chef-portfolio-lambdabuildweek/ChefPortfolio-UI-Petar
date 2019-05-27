4fba1a2339986398afa5ac2ef07d0fc45de3d585
Petar Scepanovic <hiborija@gmail.com>
2019-05-27 12:16:46 +0200

    its fine now, by less compiler was turned off so thats why it didnt work

--- scripts.js
+++ scripts.js
@@ -4,17 +4,17 @@ const wrapper = document.querySelector('.wrapper');
 const callToAction = document.querySelector('.call-me');
 
 function stickyNav() {
-    if (window.scrollY >= 200) {
-        wrapper.style.paddingTop = nav.offsetHeight;
-        nav.classList.add('sticky');
-        callToAction.style.display = "block";
-    } else {
-        wrapper.style.paddingTop = 0;
-        nav.classList.remove('sticky');
-        callToAction.style.display = "none";
+  if (window.scrollY >= 200) {
+    wrapper.style.paddingTop = nav.offsetHeight;
+    nav.classList.add('sticky');
+    callToAction.style.display = 'block';
+  } else {
+    wrapper.style.paddingTop = 0;
+    nav.classList.remove('sticky');
+    callToAction.style.display = 'none';
 
 
-    }
+  }
 }
 
 
