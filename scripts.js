cbf526aa8a6f8fbaeee6ee72a9a5bd9acd2c7e5a
Petar Scepanovic <hiborija@gmail.com>
2019-05-28 14:15:26 +0200

    added smooth scroll in scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 const nav = document.querySelector('.primary-nav');
 const wrapper = document.querySelector('.wrapper');
 const callToAction = document.querySelector('.call-me');
+
 function stickyNav() {
   if (window.scrollY >= 200) {
     wrapper.style.paddingTop = nav.offsetHeight;
@@ -15,3 +16,38 @@ function stickyNav() {
 
 window.addEventListener('scroll', stickyNav);
 // const topToNav = nav.offsetTop;
+
+
+function smoothScroll(param1, duration) {
+  const target = document.querySelector(param1);
+  const targetPosition = target.getBoundingClientRect().top;
+  const startPosition = window.pageYOffset;
+  const distance = targetPosition - startPosition;
+  let startTime = null;
+
+  function animation(currentTime) {
+    if (startTime === null) startTime = currentTime;
+    const timeElapsed = currentTime - startTime;
+    const run = ease(timeElapsed, startPosition, distance, duration);
+    window.scrollTo(0, run);
+    if (timeElapsed < duration) requestAnimationFrame(animation);
+  }
+
+
+  function ease(t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return c / 2 * t * t * t + b;
+    t -= 2;
+    return c / 2 * (t * t * t + 2) + b;
+  }
+
+
+  requestAnimationFrame(animation);
+}
+
+
+const sectionLink = document.querySelector('.section-link');
+
+sectionLink.addEventListener('click', () => {
+  smoothScroll('.testimonials', 1000);
+});
