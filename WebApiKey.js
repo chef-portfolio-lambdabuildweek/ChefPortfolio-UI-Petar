41f785d4e994cc2e9ae40b7313d5f0a779ab03fb
Petar Scepanovic <hiborija@gmail.com>
2019-05-29 11:38:58 +0200

    some experimensts with api

---
+++ WebApiKey.js
@@ -0,0 +1,14 @@
+// const apiKey = '1';
+// const topHeadlinesUrl =
+//   'https://www.themealdb.com/api/json/v1/1/random.php' + apiKey;
+
+// export { apiKey, topHeadlinesUrl };
+
+
+const apiKey = '7f65a98cdbf1496088f8cb61e23fd2e8';
+const topHeadlinesUrl = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`;
+
+export {
+  apiKey,
+  topHeadlinesUrl,
+};
--- components/features.js
+++ components/features.js
@@ -1,16 +1,13 @@
 class Feature {
-    constructor {
-        this.feature = feature;
-        this.icon = document.querySelector('.fas fa-robot fa-5x');
-        this.heading = document.querySelector('.inside-of-features h3');
-        this.paragraph = document.querySelector('.inside-of-features p');
-    }
-
-};
-
+  constructor(feature) {
+    this.feature = feature;
+    this.icon = document.querySelector('.fas fa-robot fa-5x');
+    this.heading = document.querySelector('.inside-of-features h3');
+    this.paragraph = document.querySelector('.inside-of-features p');
+  }
+}
 
 
-
-let features = document.querySelectorAll('.inside-of-features');
+const features = document.querySelectorAll('.inside-of-features');
 
 features.forEach(feature => new Feature(feature));
